7784157b51d6fd2328e7812a8dcd408d2fb7ae91
Kashif Siddiqui <kashifsiddiqui740@gmail.com>
2025-06-12 10:57:07 +0530

    added comments in code

--- Difficulty: Easy/Reversing the equation/reversing-the-equation.java
+++ Difficulty: Easy/Reversing the equation/reversing-the-equation.java
@@ -2,25 +2,26 @@
 class Solution {
 
     String reverseEqn(String S) {
-        int i = S.length() - 1;
-        StringBuilder ans = new StringBuilder();
-
+        int i = S.length() - 1; // Start from the end of the string
+        StringBuilder ans = new StringBuilder(); // This will store our final answer
+        
+        // STEP 1: Collect a full number (can be multiple digits like 20)
         while (i >= 0) {
             // Collect digits to form a number
             StringBuilder num = new StringBuilder();
             while (i >= 0 && Character.isDigit(S.charAt(i))) {
-                num.insert(0, S.charAt(i)); // Prepend digit to maintain number order
-                i--;
+                num.insert(0, S.charAt(i)); // Add digit at the beginning to keep number in correct order
+                i--; // Move to the previous character
             }
-            ans.append(num);
+            ans.append(num); // Add the number to the answer
 
-            // Append operator if any
+            // STEP 2: Add the operator (like +, -, *, /)
             if (i >= 0 && !Character.isDigit(S.charAt(i))) {
-                ans.append(S.charAt(i));
+                ans.append(S.charAt(i)); // Add the operator to the answer
                 i--;
             }
         }
 
-        return ans.toString();
+        return ans.toString(); // Return the reversed equation
     }
 }
