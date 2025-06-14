9f7c82dfaecc270f1558c3b9c703226144de43cd
Kashif Siddiqui <kashifsiddiqui740@gmail.com>
2025-06-14 18:10:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.java
+++ Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.java
@@ -1,25 +1,13 @@
 class Solution {
     public static void print_divisors(int n) {
-        // To store divisors in order
-        List<Integer> divisors = new ArrayList<>();
         
-        // Loop till sqrt(n)
-        for (int i = 1; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                divisors.add(i); // i is a divisor
-                if (i != n / i) {
-                    divisors.add(n / i); // n / i is also a divisor (if not same)
-                }
+        // If i is completely dividing n then it is a divisor of n
+        
+        for(int i=1; i<=n; i++){
+            
+            if(n % i == 0){
+                System.out.print(i + " ");
             }
         }
-        
-        // Sort the list to get ascending order
-        Collections.sort(divisors);
-        
-        // Print the divisors
-        for (int divisor : divisors) {
-            System.out.print(divisor + " ");
-        }
     }
-        
-    }
+}
