59ec9949fdd17e23e103d86d729210586eb251b2
Kashif Siddiqui <kashifsiddiqui740@gmail.com>
2025-06-14 17:51:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Armstrong Numbers/armstrong-numbers.java
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.java
@@ -4,12 +4,16 @@ class Solution {
         int original = n;
         int sum = 0;
         
-        while(n>0){
-            int digit = n % 10; // extract the last digit from n
-            n = n / 10;   // remove last digit from n
-            sum = sum + (digit * digit * digit);
-            
+        while( n > 0){
+            int lastDigit = n % 10;
+            sum += lastDigit * lastDigit * lastDigit;
+            n = n/10;
         }
-        return sum == original;     // check if the original and sum is same
-}
+        
+        if( original == sum){
+            return true;
+        } else{
+            return false;
+        }
+    }
 }
