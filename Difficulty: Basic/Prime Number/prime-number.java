7b577681173ad7cd365ade70b9adc3588f563ce9
Kashif Siddiqui <kashifsiddiqui740@gmail.com>
2025-06-15 16:13:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Prime Number/prime-number.java
+++ Difficulty: Basic/Prime Number/prime-number.java
@@ -1,17 +1,23 @@
 class Solution {
     static boolean isPrime(int n) {
-        // If 1 or less it is not prime
+       int count = 0;
+       for(int i=1; i*i<=n; i++){
+           if(n%i == 0){
+               count++;
+               if((n/i) != i){
+                   count++;
+               }
+           }
+           
+         }
+         
+         if(count == 2){
+               return true;
+           }
+           else{
+               return false;
+           
+       }
         
-        if (n <= 1){
-            return false;
-        }
-        
-        // check divisibility from 2 to the square root of n
-        for(int i=2; i<=Math.sqrt(n); i++){
-            if(n % i == 0){
-                return false;
-            }
-        }
-        return true;
     }
 }
