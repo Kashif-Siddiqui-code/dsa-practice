0140c92f883b7fe560491cdebfb381b4580978b7
Kashif Siddiqui <kashifsiddiqui740@gmail.com>
2025-06-24 23:00:03 +0530

    Time: 0 ms (100%), Space: 50.4 MB (40.99%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,26 +1,35 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        // Naive Solution using Bruteforce Approach
-        if(nums.length == 1){
+        // Binray Search
+        int n = nums.length;
+        if(n == 1){ // if array has single element return first element
             return nums[0];
         }
-        for(int i=0; i<nums.length; i++){
-            if(i == 0){                     // if element is at the first index and single
-                if(nums[i] != nums[i+1]){
-                    return nums[i];
-                }
+        if(nums[0] != nums[1]){ // if first element is single
+            return nums[0];
+        }
+        if(nums[n-1] != nums[n-2]){  // if last element is single
+            return nums[n-1];
+        }
+        int start = 1;
+        int end = nums.length-2;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            // the element is single if the prev and next element are not equal to current element
+            if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1]){
+                return nums[mid];
             }
-            else if(i == nums.length-1){    // if element is at the last index and single
-                if(nums[i] != nums[i-1]){
-                    return nums[i];
-                }
+            // we are in left half 
+            if((mid%2 == 1 && nums[mid] == nums[mid-1]) || (mid%2 == 0 && nums[mid] == nums[mid+1])){
+                start = mid + 1;
             }
-            else{ // check if element at the current index matches to the prev index or next index
-                if(nums[i] != nums[i+1] && nums[i] != nums[i-1]){
-                    return nums[i];
-                }
+            // we are on right half
+            else{
+                end = mid - 1;
             }
         }
+
         return -1;
     }
 }
