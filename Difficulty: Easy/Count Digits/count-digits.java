360dc4a1b2fc3c4415f568ddb77e6b04c56aaa4d
Kashif Siddiqui <kashifsiddiqui740@gmail.com>
2025-07-01 14:26:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count Digits/count-digits.java
+++ Difficulty: Easy/Count Digits/count-digits.java
@@ -1,10 +1,11 @@
 class Solution {
     public int countDigits(int n) {
-       int count = 0;
-       while(n > 0){
-           count = count+1;
-           n = n/10;
-       }
-       return count;
+        int count = 0;
+        while(n>0){
+          count = count + 1;
+          n = n/10;
+        }
+        return count;
+        
     }
 }
