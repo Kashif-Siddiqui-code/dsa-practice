c19d1c96b4af2bcba1f8d5df779e5b7163785ac4
Kashif Siddiqui <kashifsiddiqui740@gmail.com>
2025-07-03 18:21:14 +0530

    Time: 0 ms (100%), Space: 42.9 MB (81.97%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,23 +1,22 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
+        // we can solve this problem using the code to find the lower bound
         int start = 0;
-        int end = nums.length - 1;
+        int end = nums.length-1;
+        int ans = nums.length;
         
-        while( start <= end){
+        while(start <= end){
             int mid = start + (end - start)/2;
-
-            if(nums[mid] == target){
-                return mid;
-            }
-            else if(nums[mid] < target){
-                start = mid + 1;
+            
+            if(nums[mid] >= target){
+                ans = mid;  // possible answer
+                end = mid - 1;  // search on left side
             }
             else{
-                end = mid - 1;
+                start = mid + 1;  // search on right side
             }
+            
         }
-        // target not found, return the insert position
-        // After the loop ends, start is the position where the target should be inserted to maintain the sorted order
-        return start;
+        return ans;
     }
 }
