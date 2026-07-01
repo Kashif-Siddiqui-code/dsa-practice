207fc53b90f40e19cdcd140e397bcc98d45ce458
Kashif Siddiqui <kashifsiddiqui740@gmail.com>
2026-07-01 23:55:54 +0530

    Time: 0 ms (100%), Space: 44.3 MB (49.88%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -8,16 +8,19 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ // Approach:Three Pointers - Traverse the linked list once while reversing each node's next pointer. TC - O(n) SC - O(1)
+
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
 
         while(curr != null){
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+            ListNode next = curr.next; // save the next node
+            curr.next = prev; // reverse the pointer
+            prev = curr; // Move prev
+            curr = next; // Move curr
         }
         return prev;
     }
