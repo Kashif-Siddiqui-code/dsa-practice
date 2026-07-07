d903e2f2d4404495b2269d968493cc67433fbde7
Kashif Siddiqui <kashifsiddiqui740@gmail.com>
2026-07-07 23:19:41 +0530

    Time: 4 ms (51.05%), Space: 47.1 MB (39.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,11 +1,15 @@
+// HashMap - O(n) - find needed element - If needed in hashmap - return needed , else store curr
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[] { i, j };
-                }
+            int needed = target - nums[i];
+
+            if(map.containsKey(needed)){
+                return new int[]{map.get(needed), i};
             }
+            map.put(nums[i], i);
+
         }
         return new int[]{};
     }
